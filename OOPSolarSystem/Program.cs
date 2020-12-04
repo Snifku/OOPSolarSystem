04b1af05cc212245f293465f3cb5123373f8318a
Opilane TTHK <opilane@tthk.ee>
2020-12-04 09:10:11 +0200

    first commit

---
+++ OOPSolarSystem/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SolarSystem
+{
+    class Program
+    {
+        public class Item
+        {
+            string name;
+            int mass;
+
+
+            public Item(string _name, int _mass)
+            {
+                name = _name;
+                mass = _mass;
+                Console.WriteLine($"Item {name} created.");
+            }
+
+            public string Name { get { return name; } }
+            public int Mass { get { return mass; } }
+
+        }
+        static void Main(string[] args)
+        {
+            Planet();
+        }
+
+        public static void Planet()
+        {
+            string filePath = @"C:\Users\opilane\samples";
+            string fileName = @"planets.txt";
+            List<Item> planetItems = new List<Item>();
+            List<string> linesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+
+            foreach (string line in linesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '$' }, StringSplitOptions.RemoveEmptyEntries);
+                //Console.WriteLine($"Item: {tempArray[0]}");
+                //Console.WriteLine($"Mass: {tempArray[1]}");
+                Item newItem = new Item(tempArray[0], Int32.Parse(tempArray[1]));
+                planetItems.Add(newItem);
+
+
+
+            }
+            Console.WriteLine("your planets: ");
+            int total = 0;
+
+            foreach (Item item in planetItems)
+            {
+                Console.WriteLine($"Item: {item.Name}; Mass: {item.Mass}");
+                total += item.Mass;
+            }
+
+            Console.WriteLine($"Your planets: ");
+
+        }
+
+
+    }
+}
